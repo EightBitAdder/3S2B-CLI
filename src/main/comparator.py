bab90edeced7158cd26d71e3bd823aaf0e6bdb31
BigRe <bigredjapan411@gmail.com>
2025-05-25 19:57:43 -0400

    Add comparator.py

---
+++ src/main/comparator.py
@@ -0,0 +1,199 @@
+from typing import Tuple, Dict
+from file_readers.utils import (
+    FileType,
+    TypedDataFrame,
+    FileReader,
+    DelimitedFileReader,
+    MassListReader,
+    MSDataReader
+)
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
+
+
+class MassList():
+
+
+    def __init__(self, df: pd.DataFrame, weighted: bool):
+
+        self.df       = df
+        self.weighted = weighted
+        self.masses   = df.iloc[:, 0].to_numpy()
+        self.mults    = df.iloc[:, 1].to_numpy() if df.shape[1] > 1 else np.ones(len(df))
+
+
+    @classmethod
+    def fromFile(cls, filePath: str) -> "MassList":
+
+        typedDF = MassListReader(filePath).read()
+        weighted = (typedDF.type == FileType.MASS_LIST_DATA_DOBULE)
+
+        return cls(typedDF.df, weighted)
+
+
+class MSData():
+
+
+    def __init__(self, df: pd.DataFrame):
+
+        self.df          = df
+        self.masses      = df.iloc[:, 0].to_numpy()
+        self.intensities = df.iloc[:, 1].to_numpy()
+
+
+    @classmethod
+    def fromFile(cls, filePath: str) -> "MSData":
+
+        typedDF = MSDataReader(filePath).read()
+
+        return cls(typedDF.df)
+
+
+class Comparator():
+
+	
+	def __init__(self, msData: MSData, massList: MassList, tol: float):
+
+		self.msData   = msData
+		self.massList = massList
+		self.tol      = tol
+
+		if (tol == 0):
+
+			self.msDataMasses   = np.unique(np.round(msData.masses).astype(int))
+			self.massListMasses = np.round(massList.masses).astype(int)
+
+		else:
+
+			self.msDataMasses   = np.unique(np.round(msData.masses, 2))
+			self.massListMasses = np.round(massList.masses, 2)
+
+		self.weights, self.massesMultsDict = self._fetchWeights()
+
+
+	def _fetchWeights(self):
+
+		massesMultsDict = {}
+
+		for mass, mult in zip(self.massListMasses, self.massList.mults):
+
+			if ((mass not in massesMultsDict) or (mult < massesMultsDict[mass])):
+
+				massesMultsDict[mass] = mult
+
+		masses  = np.array(list(massesMultsDict.keys()))
+		weights = np.array([
+			np.exp(-(0.01 * massesMultsDict[m])) if self.massList.weighted else 1.0
+			for m in masses
+		])
+
+		return weights, massesMultsDict
+
+
+	def calculateFPIE(self) -> Tuple[float, Dict]:
+
+		matchedWeights = {}
+		matchedMults   = {}
+		masses         = np.array(list(self.massesMultsDict.keys()))
+
+		for mz in self.msDataMasses:
+
+			for i, m in enumerate(masses):
+
+				if (m - self.tol <= mz <= m + self.tol):
+
+					matchedMults[mz]   = self.massesMultsDict[m]
+					matchedWeights[mz] = self.weights[i]
+
+					break
+
+
+		explainedIntensities = []
+
+		for mz, intensity in zip(self.msDataMasses, self.msData.intensities):
+
+			if (mz in matchedWeights):
+
+				explainedIntensities.append(intensity * matchedWeights[mz])
+
+		FPIEScore = float(sum(explainedIntensities) / np.sum(self.msData.intensities))
+
+		return FPIEScore, {
+			"msDataMasses": self.msDataMasses,
+			"msDataIntensities": self.msData.intensities,
+			"matchedMults": matchedMults,
+			"massesMultsDict": self.massesMultsDict,
+			"weights": self.weights,
+			"matchedWeights": matchedWeights
+		}
+
+	def plotFPIE(self, plotMetaData: Dict, FPIEScore: float, title: str="") -> None:
+
+		fig, ax = plt.subplots()
+
+		fig.patch.set_facecolor("darkblue")
+		ax.set_facecolor("white")
+
+		mults         = sorted(set(plotMetaData["massesMultsDict"].values()))
+		colorMap      = cm.get_cmap("tab20b", len(mults))
+		multsColorMap = {m: colorMap(i) for i, m in enumerate(mults)}
+
+		for i, mz in enumerate(plotMetaData["msDataMasses"]):
+
+			intensity      = plotMetaData["msDataIntensities"][i]
+			color          = multsColorMap.get(
+				plotMetaData["matchedMults"].get(mz, None),
+				"black"
+			)
+			_, stemline, _ = ax.stem(
+				[mz],
+				[intensity],
+				linefmt=color,
+				markerfmt=" ",
+				basefmt=" "
+			)
+
+			if (mz not in plotMetaData["matchedWeights"]):
+
+				ax.text(
+					mz,
+					intensity + intensity * 0.05,
+					"*",
+					color="red",
+					ha="center",
+					fontsize=10
+				)
+
+		ax.set_xlabel("m/z", color="yellow")
+		ax.set_ylabel("Intensity", color="yellow")
+		ax.set_title(f"{title} FPIE Annotated Plot", color="yellow")
+		ax.grid(True, linestyle="--", linewidth=1, color="black", alpha=0.5)
+
+		legendHandles = [
+			Patch(
+				color=multsColorMap[m],
+				label=f"{int(m)}"
+			)
+			for m in mults
+		]
+
+		legendHandles.append(
+			Line2D(
+				[0],
+				[0],
+				marker="*",
+				color="red",
+				linestyle="None",
+				markersize=10,
+				label="Unexplained"
+			)
+		)
+
+		ax.legend(handles=legendHandles, title="Multiplicity")
+		ax.tick_params(axis="both", colors="yellow")
+
+		plt.show()
--- src/main/utils.py
+++ src/main/utils.py
@@ -2,61 +2,14 @@ from db.utils import (
     viewIdxTable,
     searchAndFetch
 )
-from file_readers.utils import (
-    FileType,
-    TypedDataFrame,
-    FileReader,
-    DelimitedFileReader,
-    MassListReader,
-    MSDataReader
+from main.comparator import (
+    MassList,
+    MSData,
+    Comparator
 )
 import os
-import csv
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from matplotlib.patches import Patch
-from matplotlib.lines import Line2D
 from tqdm import tqdm
-
-
-class MassList():
-
-
-    def __init__(self, df: pd.DataFrame, weighted: bool):
-
-        self.df       = df
-        self.weighted = weighted
-        self.masses   = df.iloc[:, 0].to_numpy()
-        self.mults    = df.iloc[:, 1].to_numpy() if df.shape[1] > 1 else np.ones(len(df))
-
-
-    @classmethod
-    def fromFile(cls, filePath: str) -> "MassList":
-
-        typedDF = MassListReader(filePath).read()
-        weighted = (typedDF.type == FileType.MASS_LIST_DATA_DOBULE)
-
-        return cls(typedDF.df, weighted)
-
-
-class MSData():
-
-
-    def __init__(self, df: pd.DataFrame):
-
-        self.df          = df
-        self.masses      = df.iloc[:, 0].to_numpy()
-        self.intensities = df.iloc[:, 1].to_numpy()
-
-
-    @classmethod
-    def fromFile(cls, filePath: str) -> "MSData":
-
-        typedDF = MSDataReader(filePath).read()
-
-        return cls(typedDF.df)
 
 
 def fetchMassList(allFragsDF: pd.DataFrame) -> MassList:
@@ -73,51 +26,6 @@ def fetchMassList(allFragsDF: pd.DataFrame) -> MassList:
     df = pd.DataFrame(massList, columns=["Mass", "Multiplicity"])
 
     return MassList(df, weighted=True)
-
-
-def compare(msDataPath: str, massListPath: str, *, tol: float) -> float:
-    
-    msData                  = MSData.fromFile(msDataPath)
-    massList                = MassList.fromFile(massListPath)
-    FPIEScore, plotMetaData = FPIE(
-        msData,
-        massList,
-        tol=tol,
-        weighted=massList.weighted
-    )
-
-    plotFPIE(
-        plotMetaData,
-        FPIEScore,
-        os.path.splitext(os.path.basename(msDataPath))[0]
-    )
-
-    return FPIEScore
-
-
-def compareAll(msDataPath: str, *, tol: float) -> pd.DataFrame:
-
-    msData          = MSData.fromFile(msDataPath)
-    FPIEs           = []
-    idxTableDF      = viewIdxTable()
-    craftsLabEntrys = idxTableDF.iloc[:, 1]
-
-    for entry in tqdm(craftsLabEntrys, desc=f"<*> Calculating FPIEs . . ."):
-
-        massList      = fetchMassList(searchAndFetch(entry))
-        FPIEScore, _  = FPIE(
-            msData,
-            massList,
-            tol=tol,
-            weighted=massList.weighted
-        )
-        
-        FPIEs.append(FPIEScore)
-
-    df = pd.concat([idxTableDF.iloc[:, 0], pd.DataFrame({"FPIE": FPIEs})],
-                     axis=1)
-
-    return df
 
 
 def searchAndFetchByMass(mz: float) -> pd.DataFrame:
@@ -138,143 +46,33 @@ def searchAndFetchByMass(mz: float) -> pd.DataFrame:
     return idxTableDF.loc[filteredRows].reset_index(drop=True)
 
 
-def plotFPIE(plotMetaData: dict, FPIEScore: float, title: str="") -> None:
+def compare(msDataPath: str, massListPath: str, *, tol: float) -> float:
+    
+    msData                  = MSData.fromFile(msDataPath)
+    massList                = MassList.fromFile(massListPath)
+    comparator              = Comparator(msData, massList, tol)
+    FPIEScore, plotMetaData = comparator.calculateFPIE()
 
-        msDataMasses      = plotMetaData["msDataMasses"]
-        msDataIntensities = plotMetaData["msDataIntensities"]
-        matchedWeights    = plotMetaData["matchedWeights"]
-        matchedMults      = plotMetaData["matchedMults"]
-        massesMultDict    = plotMetaData["massesMultDict"]
+    comparator.plotFPIE(plotMetaData, FPIEScore, os.path.splitext(os.path.basename(msDataPath))[0])
 
-        fig, ax = plt.subplots()
-
-        fig.patch.set_facecolor("darkblue")
-        ax.set_facecolor("white")
-
-        mults        = sorted(set(massesMultDict.values()))
-        numColors    = len(mults)
-        colorMap     = cm.get_cmap("tab20b", numColors)
-        multColorMap = {mult: colorMap(i) for i, mult in enumerate(mults)}
-
-        for i, mz in enumerate(msDataMasses):
-
-            intensity = msDataIntensities[i]
-
-            if (mz in matchedWeights):
-
-                mult  = matchedMults.get(mz, 1)
-                color = multColorMap[mult]
-
-            else:
-
-                color = "black"
-
-            _, stemline, _ = ax.stem([mz], [intensity],
-                                     linefmt=color,
-                                     markerfmt=" ",
-                                     basefmt=" ")
-
-            stemline.set_linewidth(1)
-
-            if (mz not in matchedWeights):
-
-                ax.text(mz,
-                        intensity + intensity * 0.05,
-                        "*",
-                        color="red",
-                        ha="center",
-                        fontsize=10,
-                        clip_on=True)
-
-        ax.set_xlabel("m/z", color="yellow")
-        ax.set_ylabel("Intensity", color="yellow")
-        ax.set_title(f"{title} FPIE Annotated Plot", color="yellow")
-        ax.grid(True, linestyle='--', linewidth=1, color='black', alpha=0.5)
-
-        legendHandles     = [
-            Patch(color=multColorMap[mult], label=f"{int(mult)}")
-            for mult in mults
-        ]
-        unexplainedHandle = Line2D([0],
-                                   [0],
-                                   marker="*",
-                                   color="red",
-                                   linestyle="None",
-                                   markersize=10,
-                                   label="Unexplained")
-
-        legendHandles.append(unexplainedHandle)
-
-        ax.legend(handles=legendHandles, title="Multiplicity")
-        ax.tick_params(axis="both", colors="yellow")
-
-        plt.show()
+    return FPIEScore
 
 
-def FPIE(msData: MSData,
-         massList: MassList,
-         *,
-         tol: float,
-         weighted: bool=False) -> float:
+def compareAll(msDataPath: str, *, tol: float) -> pd.DataFrame:
 
-    masses       = massList.masses
-    mults        = massList.mults
-    msDataMasses = np.unique(msData.masses)
+    msData          = MSData.fromFile(msDataPath)
+    idxTableDF      = viewIdxTable()
+    craftsLabEntrys = idxTableDF.iloc[:, 1]
+    FPIEs           = []
 
-    if (tol == 0):
+    for entry in tqdm(craftsLabEntrys, desc=f"<*> Calculating FPIEs . . ."):
 
-        masses = masses.round().astype(int)
+        massList     = fetchMassList(searchAndFetch(entry))
+        comparator   = Comparator(msData, massList, tol)
+        FPIEScore, _ = comparator.calculateFPIE()
 
-    else:
+        FPIEs.append(FPIEScore)
 
-        masses       = np.round(masses, 2)
-        msDataMasses = np.round(msDataMasses, 2)
+    df = pd.concat([idxTableDF.iloc[:, 0], pd.DataFrame({"FPIE": FPIEs})], axis=1)
 
-    massesMultDict = {}
-
-    for mass, mult in zip(masses, mults):
-
-        if ((mass not in massesMultDict) or (mult < massesMultDict[mass])):
-
-            massesMultDict[mass] = mult
-
-    masses         = np.array(list(massesMultDict.keys()))
-    weights        = np.array([
-        np.exp(-(0.01 * massesMultDict[m])) if weighted else 1.0
-        for m in masses
-    ])
-    matchedMults   = {}
-    matchedWeights = {}
-
-    for msDataMass in msDataMasses:
-
-        for i, m in enumerate(masses):
-
-            if (m - tol <= msDataMass <= m + tol):
-
-                matchedMults[msDataMass]   = massesMultDict[m]
-                matchedWeights[msDataMass] = weights[i]
-
-                break
-
-    explainedIntensities = []
-
-    for mz, intensity in zip(msData.masses, msData.intensities):
-
-        mzKey = round(mz) if tol == 0 else np.round(mz, 2)
-
-        if (mzKey in matchedWeights):
-
-            explainedIntensities.append(intensity * matchedWeights[mzKey])
-
-    msDataIntensities = msData.intensities
-    FPIEScore         = float(sum(explainedIntensities) / np.sum(msDataIntensities))
-
-    return FPIEScore, {
-        "msDataMasses": msDataMasses,
-        "msDataIntensities": msDataIntensities,
-        "matchedWeights": matchedWeights,
-        "matchedMults": matchedMults,
-        "massesMultDict": massesMultDict,
-        "weights": weights
-    }
+    return df
